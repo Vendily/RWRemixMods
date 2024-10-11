381798da0549a3bb864029cfd496434e44576881
Vendily <vendily1001@gmail.com>
2024-10-11 17:11:31 -0400

    Updated BiteDemCentipedes to 1.0.1
    Fixes bug where holding a dead centipede in the left (0) hand and a live one in the right (1) hand makes the slugcat eat the dead centipede instead of kill the right.
    
    Adjusted plugin code to only run at a full stomach.

--- BiteDemCentipedes/src/Plugin.cs
+++ BiteDemCentipedes/src/Plugin.cs
@@ -15,7 +15,7 @@ sealed class Plugin : BaseUnityPlugin
 {
     private const string PLUGIN_GUID = "vendily.bitedemcentipedes";
     private const string PLUGIN_NAME = "Bite Dem Centipedes";
-    private const string PLUGIN_VERSION = "0.1.0";
+    private const string PLUGIN_VERSION = "1.0.1";
     bool init;
 
     public void OnEnable()
@@ -23,6 +23,7 @@ sealed class Plugin : BaseUnityPlugin
         // Add hooks here
         On.RainWorld.OnModsInit += OnModsInit;
         On.Player.GrabUpdate += Player_GrabUpdate;
+        On.Player.BiteEdibleObject += Player_BiteEdibleObject;
     }
 
     private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
@@ -30,35 +31,34 @@ sealed class Plugin : BaseUnityPlugin
         orig(self);
         if (init) return;
         init = true;
-        // Initialize assets, your mod config, and anything that uses RainWorld here
-        Logger.LogDebug("Hello world!");
     }
 
     private void Player_GrabUpdate(On.Player.orig_GrabUpdate orig, Player self, bool eu)
     {
         if (!ModManager.MSC || (self.SlugCatClass != MoreSlugcatsEnums.SlugcatStatsName.Spear && self.SlugCatClass != MoreSlugcatsEnums.SlugcatStatsName.Saint))
         {
-            bool _not_moving_jumping_throwing = ((self.input[0].x == 0 && self.input[0].y == 0 && !self.input[0].jmp && !self.input[0].thrw) || (ModManager.MMF && self.input[0].x == 0 && self.input[0].y == 1 && !self.input[0].jmp && !self.input[0].thrw && (self.bodyMode != Player.BodyModeIndex.ClimbingOnBeam || self.animation == Player.AnimationIndex.BeamTip || self.animation == Player.AnimationIndex.StandOnBeam))) && (self.mainBodyChunk.submersion < 0.5f || self.isRivulet);
-            int active_grasp_index = -1;
-            if (_not_moving_jumping_throwing)
+            if (self.FoodInStomach >= self.MaxFoodInStomach)
             {
-                int i = 0;
-                while (active_grasp_index < 0 && i < 2 && (!ModManager.MSC || self.SlugCatClass != MoreSlugcatsEnums.SlugcatStatsName.Spear))
+                bool _not_moving_jumping_throwing = ((self.input[0].x == 0 && self.input[0].y == 0 && !self.input[0].jmp && !self.input[0].thrw) || (ModManager.MMF && self.input[0].x == 0 && self.input[0].y == 1 && !self.input[0].jmp && !self.input[0].thrw && (self.bodyMode != Player.BodyModeIndex.ClimbingOnBeam || self.animation == Player.AnimationIndex.BeamTip || self.animation == Player.AnimationIndex.StandOnBeam))) && (self.mainBodyChunk.submersion < 0.5f || self.isRivulet);
+                int active_grasp_index = -1;
+                if (_not_moving_jumping_throwing)
                 {
-                    if (self.grasps[i] != null && self.grasps[i].grabbed is IPlayerEdible && (self.grasps[i].grabbed as IPlayerEdible).Edible)
+                    int i = 0;
+                    while (active_grasp_index < 0 && i < 2)
                     {
-                        active_grasp_index = i;
+                        if (self.grasps[i] != null && (self.grasps[i].grabbed is Centipede) && (self.grasps[i].grabbed as Centipede).Small && !(self.grasps[i].grabbed as Centipede).dead)
+                        {
+                            active_grasp_index = i;
+                            break;
+                        }
+                        i++;
                     }
-                    i++;
-                }
-                if (active_grasp_index > -1 && self.wantToPickUp < 1 && (self.input[0].pckp || self.eatCounter <= 15) && self.Consious && Custom.DistLess(self.mainBodyChunk.pos, self.mainBodyChunk.lastPos, 3.6f))
-                {
-                    if (self.graphicsModule != null)
+                    if (active_grasp_index > -1 && self.wantToPickUp < 1 && (self.input[0].pckp || self.eatCounter <= 15) && self.Consious && Custom.DistLess(self.mainBodyChunk.pos, self.mainBodyChunk.lastPos, 3.6f))
                     {
-                        (self.graphicsModule as PlayerGraphics).LookAtObject(self.grasps[active_grasp_index].grabbed);
-                    }
-                    if (self.grasps[active_grasp_index].grabbed is Centipede && (self.grasps[active_grasp_index].grabbed as Centipede).Small && !(self.grasps[active_grasp_index].grabbed as Centipede).dead)
-                    {
+                        if (self.graphicsModule != null)
+                        {
+                            (self.graphicsModule as PlayerGraphics).LookAtObject(self.grasps[active_grasp_index].grabbed);
+                        }
                         if (self.eatCounter < 1)
                         {
                             self.eatCounter = 15;
@@ -69,6 +69,31 @@ sealed class Plugin : BaseUnityPlugin
             }
         }
         orig(self, eu);
+    }
+
+    private void Player_BiteEdibleObject(On.Player.orig_BiteEdibleObject orig, Player self, bool eu)
+    {
+        int active_grasp_index = -1;
+        for (int i = 0; i < 2; i++)
+        {
+            if (self.grasps[i] != null && (self.grasps[i].grabbed is Centipede) && (self.grasps[i].grabbed as Centipede).Small && !(self.grasps[i].grabbed as Centipede).dead)
+            {
+                active_grasp_index = i;
+                break;
+            }
+        }
+        if (self.FoodInStomach >= self.MaxFoodInStomach && active_grasp_index > -1 && (!ModManager.MSC || (self.SlugCatClass != MoreSlugcatsEnums.SlugcatStatsName.Spear && self.SlugCatClass != MoreSlugcatsEnums.SlugcatStatsName.Saint)))
+        {
+            (self.grasps[active_grasp_index].grabbed as Creature).SetKillTag(self.abstractCreature);
+            if (self.graphicsModule != null)
+            {
+                (self.graphicsModule as PlayerGraphics).BiteFly(active_grasp_index);
+            }
+            (self.grasps[active_grasp_index].grabbed as IPlayerEdible).BitByPlayer(self.grasps[active_grasp_index], eu);
+        } else
+        {
+            orig(self, eu);
+        }
     }
 
 }
